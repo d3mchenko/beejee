745acf41e5d6c3ca361c0ff371e9d7395d6fad05
Maksim <maksim-ignatyuk@mail.ru>
2022-08-25 03:23:09 +0300

    delete todo-frontend

--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-class Config {
-    getBaseUrl() {
-        return process.env.REACT_APP_NODE_ENV === 'production' ? process.env.REACT_APP_BASE_URL_PRODUCTION : process.env.REACT_APP_BASE_URL;
-    }
-}
-
-export default new Config();
